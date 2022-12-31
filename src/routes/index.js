43bfe31c8330743cb749f68f4301a8824c15078e
tienduc205 <tienduc20052000@gmail.com>
2022-12-31 13:56:49 +0700

    Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import { DefaultLayout, HeaderOnly } from '~/components/Layout';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+export const puclicRoutes = [
+    { path: '/', component: Home, layout: DefaultLayout },
+    { path: '/following', component: Following, layout: DefaultLayout },
+    { path: '/profile', component: Profile, layout: DefaultLayout },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search },
+]
+export const privateRoutes = [
+
+]
