9c8143d0ad23482d6f12439ffbb795ff834c93e5
tienduc205 <tienduc20052000@gmail.com>
2023-01-10 13:58:08 +0700

    101. Xây dựng UI phần Header #5

--- src/components/Layout/component/Header/index.js
+++ src/components/Layout/component/Header/index.js
@@ -16,6 +16,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English'
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                }
+            ]
+        }
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -37,9 +52,20 @@ function Header() {
     const [searchResult, setSearchResult] = useState([])
     useEffect(() => {
         setTimeout(() => {
-            setSearchResult([1])
+            setSearchResult([])
         }, 0)
     }, [])
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                console.log(menuItem.code);
+                break;
+
+            default:
+                break;
+        }
+    }
+
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
             <div className={cx('logo')}>
@@ -81,7 +107,7 @@ function Header() {
                 </Button>
                 <Button primary  >Log in</Button>
 
-                <Menu items={MENU_ITEMS}>
+                <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                     <button className={cx('more-btn')}>
                         <FontAwesomeIcon icon={faEllipsisVertical} />
                     </button>
--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -2,9 +2,9 @@ import Button from "~/components/Button";
 import classNames from 'classnames/bind';
 import styles from './Menu.module.scss'
 const cx = classNames.bind(styles)
-function MenuItem({ key, data }) {
+function MenuItem({ key, data, onClick }) {
     return (
-        <Button className={cx('menu-item')} key={key} icon={data.icon} >
+        <Button className={cx('menu-item')} key={key} to={data.to} icon={data.icon} onClick={onClick} >
             {data.title}
         </Button>
     );
